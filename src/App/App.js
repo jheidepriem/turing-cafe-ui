1dca4981dd0d6862dbc69d621e2a5b3968718b75
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-21 10:36:57 -0600

    Install file dependencies

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import { fetchApiData } from './Api';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+   }
+   componentDidMount = () => {
+    fetchApiData()
+    .then((data) => this.setState({reservations: data}))
+    .catch((err) => console.log(err, 'Sorry, there has been an error!'))
+  }
   render() {
     return (
       <div className="App">
