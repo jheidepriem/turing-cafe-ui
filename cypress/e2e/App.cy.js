f13ed28ba9b1fa0e6eb81e16d2ce8e3161978e6c
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-21 12:41:59 -0600

    Change test name

--- cypress/e2e/App.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Turing Cafe', () => {
-  
-    beforeEach(() => {
-      cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {fixture: 'library.json'})
-      cy.visit('http://localhost:3000/')
-    });
-    
-  })
