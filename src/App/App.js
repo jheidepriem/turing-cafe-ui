38bdc435d52ec5d032fc1667ed62271d7cf23f75
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-21 11:56:19 -0600

    Add resyForm component and addReservation function to app

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { fetchApiData } from './Api';
 import './App.css';
 import Reservations from './Reservations';
+import ResyForm from './ResyForm';
 
 class App extends Component {
   constructor() {
@@ -12,15 +13,23 @@ class App extends Component {
    }
    componentDidMount = () => {
     fetchApiData()
-    .then((data) => this.setState({reservations: data}))
+    .then((data) => this.setState({
+      reservations: data
+    }))
     .catch((err) => console.log(err, 'Sorry, there has been an error!'))
   }
+
+  addReservation = (newReservation) => {
+    this.setState({ reservations: [...this.state.reservations, newReservation] })
+  }
+  
   render() {
     return (
-      <div className="App">
+      <main className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
+        <ResyForm addReservation={this.addReservation}/>
         <Reservations reservations={this.state.reservations}/>
-      </div>
+      </main>
       
     )
   }
